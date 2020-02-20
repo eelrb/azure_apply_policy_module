a7e3e48366345240c79e479afad73a8f4d4a5210
Brian Lee <35783300+eelrb@users.noreply.github.com>
2020-02-20 14:14:11 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-provider "tfe" {}
 
 data "terraform_remote_state" "azure-policy-demo" {
   backend = "remote"
@@ -11,24 +10,18 @@ data "terraform_remote_state" "azure-policy-demo" {
   }
 }
 
+resource "azurerm_policy_definition" "blee-policy-1" {
+  policy_1_id = 
+}
+
 
 
 /*
 
 
-resource "azurerm_policy_assignment" "requiredTag" {
-  ...
-  policy_definition_id = "${azurerm_policy_definition.requiredTag.id}"
-  ...
-}
-
 data "azurerm_subscription" "current" {}
-​
-resource "azurerm_policy_assignment" "requiredTag" {
-  ...
-  scope                = "${data.azurerm_subscription.current.id}"
-  ...
-}
+
+
 
 data "template_file" "requiredTag_policy_assign" {
   template = <<PARAMETERS
@@ -37,23 +30,19 @@ data "template_file" "requiredTag_policy_assign" {
         "value": "${var.requiredTag}"
     }
 }
-​
+
 PARAMETERS
 }
-​
-resource "azurerm_policy_assignment" "requiredTag" {
-  ...
-  parameters           = "${data.template_file.requiredTag_policy_assign.rendered}"
-}
+
 
 
 resource "azurerm_policy_assignment" "requiredTag" {
   name                 = "Audit-RequiredTag-${var.requiredTag}"
   display_name         = "Assign Required Tag '${var.requiredTag}'"
   description          = "Assignment of Required Tag Policy for '${var.requiredTag}'"
-  policy_definition_id = "???"
-  scope                = "???"
-  parameters           = "???"
+  policy_definition_id = data.terraform_remote_state.azure-policy-demo.outputs.policy_1_id
+  scope                = "${data.azurerm_subscription.current.id}"
+  parameters           = "${data.template_file.requiredTag_policy_assign.rendered}"
   
   }
   
