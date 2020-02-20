834879fedbec0160739ca0e077c31a6bf203c264
Brian Lee <35783300+eelrb@users.noreply.github.com>
2020-02-20 12:58:18 -0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,59 @@
+
+data "terraform_remote_state" "azure-policy-" {
+  backend = "remote"
+
+  config = {
+    organization = "hashicorp"
+    workspaces = {
+      name = "vpc-prod"
+    }
+  }
+}
+
+
+
+/*
+
+
+resource "azurerm_policy_assignment" "requiredTag" {
+  ...
+  policy_definition_id = "${azurerm_policy_definition.requiredTag.id}"
+  ...
+}
+
+data "azurerm_subscription" "current" {}
+​
+resource "azurerm_policy_assignment" "requiredTag" {
+  ...
+  scope                = "${data.azurerm_subscription.current.id}"
+  ...
+}
+
+data "template_file" "requiredTag_policy_assign" {
+  template = <<PARAMETERS
+{
+    "tagName": {
+        "value": "${var.requiredTag}"
+    }
+}
+​
+PARAMETERS
+}
+​
+resource "azurerm_policy_assignment" "requiredTag" {
+  ...
+  parameters           = "${data.template_file.requiredTag_policy_assign.rendered}"
+}
+
+
+resource "azurerm_policy_assignment" "requiredTag" {
+  name                 = "Audit-RequiredTag-${var.requiredTag}"
+  display_name         = "Assign Required Tag '${var.requiredTag}'"
+  description          = "Assignment of Required Tag Policy for '${var.requiredTag}'"
+  policy_definition_id = "???"
+  scope                = "???"
+  parameters           = "???"
+  
+  }
+  
+  */
