7ecd8b70244c26791f3fa7d058bd84acf7e6b71e
Brian Lee <35783300+eelrb@users.noreply.github.com>
2020-02-20 16:30:40 -0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "policy_assign_id_1" {
-  value = join("", azurerm_policy_assignment.requiredTag.*.id) 
+  value = join("", azurerm_policy_assignment.requiredTag1.*.id) 
 }
 
 output "policy_assign_id_2" {
-  value = join("", azurerm_policy_assignment.requiredTag.*.id) 
+  value = join("", azurerm_policy_assignment.requiredTag2.*.id) 
 }
 
 output "policy_assign_id_3" {
-  value = join("", azurerm_policy_assignment.requiredTag.*.id) 
+  value = join("", azurerm_policy_assignment.requiredTa3.*.id) 
 }
 
 
