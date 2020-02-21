0a3086cd3fd21a5221ac08c23989e0e92cfb6deb
Brian Lee <35783300+eelrb@users.noreply.github.com>
2020-02-20 16:32:44 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,10 +30,10 @@ PARAMETERS
 
 
 
-resource "azurerm_policy_assignment" "requiredTag" {
+resource "azurerm_policy_assignment" "blee-policy-1" {
   count                = var.policy_1_enabled
-  name                 = "Audit-RequiredTag-${var.requiredTag}"
-  display_name         = "Assign Required Tag '${var.requiredTag}'"
+  name                 = "blee-policy-1-${var.requiredTag}"
+  display_name         = "blee-policy-1 '${var.requiredTag}'"
   description          = "Assignment of Required Tag Policy for '${var.requiredTag}'"
   policy_definition_id = data.terraform_remote_state.azure-policy-demo.outputs.policy_1_id
   scope                = "${data.azurerm_subscription.current.id}"
@@ -41,10 +41,10 @@ resource "azurerm_policy_assignment" "requiredTag" {
   
   }
   
-resource "azurerm_policy_assignment" "requiredTag2" {
+resource "azurerm_policy_assignment" "blee-policy-2" {
   count                = var.policy_2_enabled
-  name                 = "Audit-RequiredTag-${var.requiredTag}"
-  display_name         = "Assign Required Tag '${var.requiredTag}'"
+  name                 = "blee-policy-2-${var.requiredTag}"
+  display_name         = "blee-policy-2 '${var.requiredTag}'"
   description          = "Assignment of Required Tag Policy for '${var.requiredTag}'"
   policy_definition_id = data.terraform_remote_state.azure-policy-demo.outputs.policy_2_id
   scope                = "${data.azurerm_subscription.current.id}"
@@ -52,10 +52,10 @@ resource "azurerm_policy_assignment" "requiredTag2" {
   
   }
 
-resource "azurerm_policy_assignment" "requiredTag3" {
+resource "azurerm_policy_assignment" "blee-policy-3" {
   count                = var.policy_3_enabled
-  name                 = "Audit-RequiredTag-${var.requiredTag}"
-  display_name         = "Assign Required Tag '${var.requiredTag}'"
+  name                 = "blee-policy-3-${var.requiredTag}"
+  display_name         = "blee-policy-3 '${var.requiredTag}'"
   description          = "Assignment of Required Tag Policy for '${var.requiredTag}'"
   policy_definition_id = data.terraform_remote_state.azure-policy-demo.outputs.policy_3_id
   scope                = "${data.azurerm_subscription.current.id}"
