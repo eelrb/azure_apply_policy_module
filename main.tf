371962116c1988c0e8763cbfbd98069d0ca9c6c1
Brian Lee <35783300+eelrb@users.noreply.github.com>
2020-02-20 16:02:51 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,4 +41,25 @@ resource "azurerm_policy_assignment" "requiredTag" {
   
   }
   
+resource "azurerm_policy_assignment" "requiredTag2" {
+  count                = var.policy_2_enabled
+  name                 = "Audit-RequiredTag-${var.requiredTag}"
+  display_name         = "Assign Required Tag '${var.requiredTag}'"
+  description          = "Assignment of Required Tag Policy for '${var.requiredTag}'"
+  policy_definition_id = data.terraform_remote_state.azure-policy-demo.outputs.policy_2_id
+  scope                = "${data.azurerm_subscription.current.id}"
+  parameters           = "${data.template_file.requiredTag_policy_assign.rendered}"
+  
+  }
+
+resource "azurerm_policy_assignment" "requiredTag3" {
+  count                = var.policy_3_enabled
+  name                 = "Audit-RequiredTag-${var.requiredTag}"
+  display_name         = "Assign Required Tag '${var.requiredTag}'"
+  description          = "Assignment of Required Tag Policy for '${var.requiredTag}'"
+  policy_definition_id = data.terraform_remote_state.azure-policy-demo.outputs.policy_3_id
+  scope                = "${data.azurerm_subscription.current.id}"
+  parameters           = "${data.template_file.requiredTag_policy_assign.rendered}"
+  
+  }
  
